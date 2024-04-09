c24110d69e6f4b9c21fd2d51dc1ca60760e44af2
Michael Rosendal <65503882+mbarosendal@users.noreply.github.com>
2024-04-09 11:30:42 +0200

    Add project files.

---
+++ Disaheim/Amulet.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disaheim
+{
+    public class Amulet
+    {
+        private string _itemId;
+        private string _design;
+        private Level _quality;
+
+        public string ItemId
+        {
+            get { return _itemId; }
+            set { _itemId = value; }
+        }
+
+        public string Design
+        {
+            get { return _design; }
+            set { _design = value; }
+        }
+
+        public Level Quality
+        {
+            get { return _quality; }
+            set { _quality = value; }
+        }
+
+        // Default constructor with one parameter.
+        public Amulet(string itemId)
+        {
+            this.ItemId = itemId;
+        }
+
+        // Constructor overload with two parameters.
+        public Amulet(string itemId, Level quality)
+        {
+            this._itemId = itemId;
+            this._design = quality.ToString();
+        }
+
+        // Constructor overload number two with three parameters.
+        public Amulet(string itemId, Level quality, string design)
+        {
+            this.ItemId= itemId;
+            //this._design = quality.ToString(); ?
+            this._design = design;  
+        }
+
+        public override string ToString()
+        {
+            return null;
+        }
+
+
+
+
+    }
+}
---
+++ Disaheim/Book.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disaheim
+{
+    public class Book
+    {
+        private string _itemId;
+        private string _title;
+        private double _price;
+
+        public string ItemId
+        {
+            get { return _itemId; }
+            set { _itemId = value; }
+        }
+
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+
+        public double Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+
+        public override string ToString()
+        {
+            return null;
+        }
+
+        public Book(string itemId)
+        {
+            _itemId = ItemId;
+        }
+
+        public Book(string itemId, string title)
+        {
+            _itemId = itemId;
+            _title = title;
+        }
+
+        public Book(string itemId, string title, double price) 
+        {
+            _itemId = itemId;
+            _title = title;
+            _price = price;
+        }
+
+
+    }
+}
